bba6a7c676abb5721e3bae339fb34238c239c47a
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2021-12-07 12:04:05 +0300

    Fix minor style issues.

--- SearchByPatternInText.Tests/SearcherTests.cs
+++ SearchByPatternInText.Tests/SearcherTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using NUnit.Framework;
 
 #pragma warning disable CA1707
@@ -8,8 +8,8 @@ namespace SearchByPatternInText.Tests
     [TestFixture]
     public class SearcherTests
     {
-        [TestCase("aababAba baabba Aba", "aba", false, ExpectedResult = new[] {2, 6, 17})]
-        [TestCase("aabababa baabba aba", "aba", true, ExpectedResult = new[] {2, 4, 6, 17})]
+        [TestCase("aababAba baabba Aba", "aba", false, ExpectedResult = new[] { 2, 6, 17 })]
+        [TestCase("aabababa baabba aba", "aba", true, ExpectedResult = new[] { 2, 4, 6, 17 })]
         [TestCase(
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
             "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
@@ -18,7 +18,9 @@ namespace SearchByPatternInText.Tests
             "remaining essentially unchanged. It was popularised in the 1960s with the release of " +
             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing " +
             "software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "Lorem", false, ExpectedResult = new[] {1, 76, 447, 563})]
+            "Lorem",
+            false,
+            ExpectedResult = new[] { 1, 76, 447, 563 })]
         [TestCase(
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
             "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
@@ -27,12 +29,16 @@ namespace SearchByPatternInText.Tests
             "remaining essentially unchanged. It was popularised in the 1960s with the release of " +
             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing " +
             "software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "int", false, ExpectedResult = new[] {43, 172, 306})]
+            "int",
+            false,
+            ExpectedResult = new[] { 43, 172, 306 })]
         [TestCase(
             "   It was popularised in the 1960s with the release of " +
             "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing " +
             "software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "it", false, ExpectedResult = new[] {4, 37, 124})]
+            "it",
+            false,
+            ExpectedResult = new[] { 4, 37, 124 })]
         [TestCase(
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
             "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
@@ -41,7 +47,9 @@ namespace SearchByPatternInText.Tests
             "remaininig essentially unchanged. It was popularised in the 1960s with the release of " +
             "Letraset sheets containinig Lorem Ipsum passages, and more recently with desktop publishing " +
             "software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "ini", true, ExpectedResult = new[] {339, 341, 442, 444})]
+            "ini",
+            true,
+            ExpectedResult = new[] { 339, 341, 442, 444 })]
         public int[] SearchPatternString_Tests(string text, string pattern, bool behind)
         {
             return text.SearchPatternString(pattern, behind);
@@ -50,14 +58,16 @@ namespace SearchByPatternInText.Tests
         [Test]
         public void SearchPatternString_TextIsNull_ThrowArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => Searcher.SearchPatternString(null, "pattern", false),
+            Assert.Throws<ArgumentException>(
+                () => Searcher.SearchPatternString(null, "pattern", false),
                 "Source string cannot be null.");
         }
 
         [Test]
         public void SearchPatternString_PatternIsNull_ThrowArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => "source text".SearchPatternString(null, false),
+            Assert.Throws<ArgumentException>(
+                () => "source text".SearchPatternString(null, false),
                 "Pattern string cannot be null.");
         }
     }
