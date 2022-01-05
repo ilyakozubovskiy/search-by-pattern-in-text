6153ee300f5cad933881e5b0cd4ab69a4f95eaa5
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-01-05 22:11:03 +0300

    Search by Pattern in Text

--- SearchByPatternInText/Searcher.cs
+++ SearchByPatternInText/Searcher.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SearchByPatternInText
 {
@@ -16,7 +17,28 @@ namespace SearchByPatternInText
         /// <exception cref="ArgumentException">Thrown if text or pattern is null.</exception>
         public static int[] SearchPatternString(this string text, string pattern, bool overlap)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (text == null)
+            {
+                throw new ArgumentException("Source string cannot be null.");
+            }
+
+            if (pattern == null)
+            {
+                throw new ArgumentException("Pattern string cannot be null.");
+            }
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i + pattern.Length <= text.Length && text.Substring(i, pattern.Length).ToUpperInvariant() == pattern.ToUpperInvariant())
+                {
+                    result.Add(i + 1);
+                    i += overlap ? 0 : pattern.Length - 1;
+                }
+            }
+
+            return result.ToArray();
         }
     }
 }
